fefcc2f1918543fac912d330e1ff6a3cc43e1d04
asm99 <asm99@REDIS4>
2023-12-16 20:50:35 +0700

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using System.Net.Mail;
+using System.IO;
+using System.Net;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(MessageModel model, List<HttpPostedFileBase> attachments)
+        {
+            using (MailMessage mm = new MailMessage(model.Email, model.To))
+            {
+                mm.Subject = model.Subject;
+                mm.Body = model.Body;
+                foreach (HttpPostedFileBase attachment in attachments)
+                {
+                    if (attachment != null)
+                    {
+                        string fileName = Path.GetFileName(attachment.FileName);
+                        mm.Attachments.Add(new Attachment(attachment.InputStream, fileName));
+                    }
+                }
+                mm.IsBodyHtml = false;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.EnableSsl = true;
+                NetworkCredential NetworkCred = new NetworkCredential(model.Email, model.Password);
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = NetworkCred;
+                smtp.Port = 587;
+                smtp.Send(mm);
+                ViewBag.Message = "Email sent.";
+            }
+
+            return View();
+        }
+    }
+}
